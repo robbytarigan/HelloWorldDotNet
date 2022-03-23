d00e01f39f567b52a67b51d7f93edd669872cc4c
Robby Tarigan <robby.tarigan@cirium.com>
2022-03-23 23:51:10 +0000

    Can over posting

--- 6.0/MinimalWebApi/TodoApi/Program.cs
+++ 6.0/MinimalWebApi/TodoApi/Program.cs
@@ -60,6 +60,7 @@ class Todo
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
 }
 
 class TodoDb : DbContext
